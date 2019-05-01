4cf155898aebfa1589baca7d715c125b3559d2eb
Galochka <viktorgalocka@gmail.com>
2019-05-01 21:39:22 +0300

    add some JS features

--- js/script.js
+++ js/script.js
@@ -1,34 +1,78 @@
+let anchor = document.getElementById("to-form");
 let prevSwitch = document.querySelector(".slider__switch-left");
 let nextSwitch = document.querySelector(".slider__switch-right");
-
-let openUp = document.querySelector(".fa-plus")
-let collapsedText = document.querySelector(".services__collapse-content")
-
-let i = 0;
-
+let openUp = document.getElementsByClassName("fa-plus")
 let images = document.querySelectorAll(".slider img");
+let form = document.querySelector("footer");
+
+let slide = 0;
+let scrolled;
+let target = form.getBoundingClientRect().top;
+
+anchor.addEventListener("click", function() {
+	scrolled = window.pageYOffset;
+	scrollToForm();
+})
+function scrollToForm() {
+	if(scrolled < target) {
+		window.scrollTo(0, scrolled);
+		scrolled += 100;
+		requestAnimationFrame(scrollToForm);
+	}
+	else {
+		// scrolled = target;
+		window.scrollTo(0, target);
+	}
+}
 
 prevSwitch.addEventListener("click", function() {
-	images[i].className = "";
-	i--;
-	if (i < 0) {
-		i = images.length - 1;
+	images[slide].className = "";
+	slide--;
+	if (slide < 0) {
+		slide = images.length - 1;
 	}
-	images[i].className = "slider__show";
+	images[slide].className = "slider__show";
 })
 
 nextSwitch.addEventListener("click", function() {
-	images[i].className = "";
-	i++;
-	if (i >= images.length) {
-		i = 0;
+	images[slide].className = "";
+	slide++;
+	if (slide >= images.length) {
+		slide = 0;
 	}
-	images[i].className = "slider__show";
+	images[slide].className = "slider__show";
 })
 
-openUp.addEventListener("click", function() {
-	collapsedText.classList.toggle("services__collapse-enable");
-	openUp.classList.toggle("fa-plus");
-	openUp.classList.toggle("fa-minus");
+for(i = 0; i < openUp.length; i++) {
+	openUp[i].addEventListener("click", function () {
+		this.classList.toggle("fa-minus");
+		this.classList.toggle("fa-plus");
+		let content = this.nextElementSibling;
+		if(content.style.maxHeight) {
+			content.style.maxHeight = null;
+			content.style.color = null;
+		} else {
+			content.style.maxHeight = content.scrollHeight + "px";
+			content.style.color = "#fff";
+		}
+	})
+}
 
-})
+let search = document.getElementsByClassName("fa-search-plus");
+
+for(i = 0; i < search.length; i++) {
+	search[i].addEventListener("click", function () {
+		let content = this.nextElementSibling;
+		let parent = this.parentNode;
+		if(content.style.maxHeight) {
+			content.style.maxHeight = null;
+			content.style.background = null;
+			parent.style.lineHeight = 257 + "px";
+			
+		} else {
+			content.style.maxHeight = content.scrollHeight + "px";
+			content.style.background = "rgba(255, 255, 255,.9)";
+			parent.style.lineHeight = 180 + "px";
+		}
+	})
+}
